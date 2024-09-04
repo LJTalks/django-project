3f5ab321ab3d0dac5eccb2814a53ff21bb1bda69
Laura Read <laurajmtb@gmail.com>
2024-09-04 16:39:38 +0000

    Prepare for Heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-57snfvi2t$rk-#o4%nn0z#i21=-b_=&jnms*whov@_33e3)tpi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ljtalks-djangoproject-9dwr0btyfgo.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-ljtalks-djangoproject-9dwr0btyfgo.ws.codeinstitute-ide.net','.herokuapp.com'
+]
 
 
 # Application definition
